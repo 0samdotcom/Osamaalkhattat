68719a3232a31cda977f1b2a4b0668998b187bf2
0samdotcom <80224727+0samdotcom@users.noreply.github.com>
2023-01-21 15:58:32 +0200

    Update SpeakGoodBye.js

--- week4/easier/SpeakGoodBye.js
+++ week4/easier/SpeakGoodBye.js
@@ -22,6 +22,6 @@ function speak(name) {
 
 // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
 // 'byeSpeaker' on the global scope as well.
-// xxxx.xxxx = byeSpeaker;
+// speakWord.speak = byeSpeaker;
 
 })(); // closing the global IIFE
