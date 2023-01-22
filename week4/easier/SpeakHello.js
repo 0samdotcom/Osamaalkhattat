e9d4792fce6bf94d3113043c21a4ccd24e4c05b6
0samdotcom <80224727+0samdotcom@users.noreply.github.com>
2023-01-22 13:11:27 +0200

    Update SpeakHello.js

--- week4/easier/SpeakHello.js
+++ week4/easier/SpeakHello.js
@@ -4,7 +4,7 @@
 (function () {
 
 
-// STEP 3: Create an object, called 'helloSpeaker' to which you will attach
+// Done? STEP 3: Create an object, called 'helloSpeaker' to which you will attach
 // the "speak" method and which you will expose to the global context
 // See Lecture 52, part 1
 
